6fe626d06ab017f1c81f3451d2568cef44dbefd0
Guillaume Smet <guillaume.smet@gmail.com>
2016-09-01 18:37:06 +0200

    Add the -DdeployAtEnd=true option to the mvn deploy command

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,4 @@ else
 	ADDITIONAL_OPTIONS=""
 fi
 
-mvn clean deploy -s $HOME/.m2/settings-search-release.xml -DskipTests=true -Dcheckstyle.skip=true -Dmaven.compiler.useIncrementalCompilation=false $ADDITIONAL_OPTIONS
+mvn clean deploy -s $HOME/.m2/settings-search-release.xml -DskipTests=true -Dcheckstyle.skip=true -DdeployAtEnd=true -Dmaven.compiler.useIncrementalCompilation=false $ADDITIONAL_OPTIONS
