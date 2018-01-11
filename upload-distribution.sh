0148c9561f79a4f376fd52d785c5c8ff7c69b1e7
Yoann Rodière <yoann@hibernate.org>
2018-01-11 14:12:04 +0100

    Support projects putting their distribution files in a distribution/target/dist directory
    ... giving a greater flexibility to projects as to which files are to be
    uploaded.

--- upload-distribution.sh
+++ upload-distribution.sh
@@ -28,12 +28,28 @@ REMOTE_DIST_URL=frs.sourceforge.net:$REMOTE_DIST_PARENT_DIR/$RELEASE_VERSION/
 
 scp README.md $REMOTE_DIST_URL
 scp changelog.txt $REMOTE_DIST_URL
-scp distribution/target/hibernate-$PROJECT-$RELEASE_VERSION-dist.zip $REMOTE_DIST_URL
-scp distribution/target/hibernate-$PROJECT-$RELEASE_VERSION-dist.tar.gz $REMOTE_DIST_URL
 
-MODULE=modules/target/hibernate-$PROJECT-modules-$RELEASE_VERSION-wildfly-10-dist.zip
-if [ -f $MODULE ]; then
-	scp $MODULE $REMOTE_DIST_URL
+# Recursive upload of the dist directory (whose content is project-specific)
+DIST_DIR=distribution/target/dist
+if [ -d $DIST_DIR ]; then
+	# Cd to the dist directory to prevent scp from uploading a "dist" directory
+	pushd $DIST_DIR
+	scp -r . $REMOTE_DIST_URL
+	popd
+fi
+
+# Legacy behavior with explicit uploads - useful for older branches
+LEGACY_DIST_ZIP=distribution/target/hibernate-$PROJECT-$RELEASE_VERSION-dist.zip
+if [ -f $LEGACY_DIST_ZIP ]; then
+	scp $LEGACY_DIST_ZIP $REMOTE_DIST_URL
+fi
+LEGACY_DIST_TAR=distribution/target/hibernate-$PROJECT-$RELEASE_VERSION-dist.tar.gz
+if [ -f $LEGACY_DIST_TAR ]; then
+	scp $LEGACY_DIST_TAR $REMOTE_DIST_URL
+fi
+LEGACY_MODULE=modules/target/hibernate-$PROJECT-modules-$RELEASE_VERSION-wildfly-10-dist.zip
+if [ -f $LEGACY_MODULE ]; then
+	scp $LEGACY_MODULE $REMOTE_DIST_URL
 fi
 
 popd
