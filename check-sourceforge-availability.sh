9a23526c9a074f32075641719ddef38eef048553
Yoann Rodière <yoann@hibernate.org>
2018-04-12 12:04:47 +0200

    Try sourceforge uploads multiple times to work around transient connection issues

--- check-sourceforge-availability.sh
+++ check-sourceforge-availability.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-((echo quit) | sftp -b - frs.sourceforge.net ) || (echo "SourceForge not available at the moment. Try again later."; exit 1)
+# See utils.sh
+source $(readlink -f ${BASH_SOURCE[0]} | xargs dirname)/utils.sh
+
+# Using /dev/null as input to just test connection, we don't want to execute anything
+try_multiple_times sftp -b /dev/null frs.sourceforge.net || (echo "SourceForge not available at the moment. Try again later."; exit 1)
